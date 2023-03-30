be220b1a67a6c686b9f9a3ed82beb69570fae521
Aditya-nb <69865078+Aditya-nb@users.noreply.github.com>
2023-03-31 00:30:53 +0530

    Add files via upload

---
+++ books-microservice/src/main/java/com/lib/main/BooksMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lib.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksMicroserviceApplication.class, args);
+	}
+
+}
---
+++ books-microservice/src/test/java/com/lib/main/BooksMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lib.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
